55adba1d46ad6172897801c33fbf2b43f351a53f
Guy Bitan <guy.bitan@gmail.com>
2020-03-25 18:36:01 +0200

    TheNewBoston nodeJs projects

---
+++ 8 - Modules/app.js
@@ -0,0 +1,6 @@
+// import movies file (without *.js extension)
+var movies = require('./movies');
+
+//Call from movies avatar (only this exported)
+movies.avatar();
+
---
+++ 9 - More on Modules/app.js
@@ -0,0 +1,7 @@
+// import movies file (without *.js extension)
+var movies = require('./movies');
+
+//Call from movies avatar (only this exported)
+movies.printAvatar();
+console.log(movies.favoriteMovie);
+
