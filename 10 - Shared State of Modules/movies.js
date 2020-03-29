bb093616295e1647fc4fbb1b9d6041d4f1f4d88e
Guy Bitan <guy.bitan@gmail.com>
2020-03-29 09:36:50 +0300

    TheNewBoston nodeJs projects

---
+++ 10 - Shared State of Modules/movies.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    printAvatar: function printAvatar() {
+        console.log("Avatar: PG-13");
+    },
+
+    printChappie: function printChappie() {
+        console.log("Chappie: R");
+    },
+    favoriteMovie: "The Matrix"
+}
